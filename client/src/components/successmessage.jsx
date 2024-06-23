b613fdd1932c38fd9cb8ada42e7bd034b63378e4
nirapyus <vjpcanaria@bpsu.edu.ph>
2024-06-24 01:40:07 +0800

    .

--- client/src/components/successmessage.jsx
+++ client/src/components/successmessage.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const SuccessMessage = ({ title, message }) => {
   return (
-    <div className={`fixed top-10 left-[50%] translate-x-[-50%] flex flex-col items-center gap-1 px-5 py-3 font-inter font-bold rounded-lg bg-green-100 text-green-200`}>
-      <h4 className="text-3xl">{title}!</h4>
-      <p className="text-xl">{message}</p>
+    <div className={`fixed top-10 left-[50%] translate-x-[-50%] flex flex-col items-center gap-1 px-5 py-3 font-inter rounded-lg bg-green-100 text-green-200`}>
+      <h4 className="font-bold text-3xl">{title}!</h4>
+      <p className="font-medium text-xl">{message}</p>
     </div>
   );
 };

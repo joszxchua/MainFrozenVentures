828f350955724d93f8314e13b0582dbd41c6a288
nirapyus <vjpcanaria@bpsu.edu.ph>
2024-06-29 11:40:49 +0800

    .

--- client/src/components/add-product.jsx
+++ client/src/components/add-product.jsx
@@ -1,12 +1,26 @@
-import React, { useRef } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { UserContext } from "../context/user-context";
+import { useForm } from "react-hook-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faIceCream, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export const AddProduct = ({ cancelAddProduct }) => {
+  const { user } = useContext(UserContext);
+  const [productImage, setProductImage] = useState(null);
+  const [productImagePreview, setProductImagePreview] = useState(null);
   const fileInputRef = useRef(null);
 
-  const handleClick = () => {
+  const handleSelectImage = () => {
     fileInputRef.current.click();
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setProductImage(file);
+      setProductImagePreview(URL.createObjectURL(file));
+    }
   };
 
   return (
@@ -21,20 +35,30 @@ export const AddProduct = ({ cancelAddProduct }) => {
       </div>
       <form>
         <div className="flex flex-col gap-5 items-center justify-center">
-          <FontAwesomeIcon
-            icon={faIceCream}
-            onClick={handleClick}
-            className="w-[200px] h-[200px] p-5 text-purple-200 shadow-2xl cursor-pointer rounded-lg"
-          />
+          {productImagePreview ? (
+            <img
+              onClick={handleSelectImage}
+              src={productImagePreview}
+              alt="Product Image"
+              className="rounded-lg w-60 h-60 object-cover"
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faIceCream}
+              onClick={handleSelectImage}
+              className="w-[200px] h-[200px] p-5 text-purple-200 shadow-2xl cursor-pointer rounded-lg"
+            />
+          )}
           <input
             type="file"
             accept="image/*"
             ref={fileInputRef}
+            onChange={handleFileChange}
             style={{ display: "none" }}
           />
           <button
             type="button"
-            onClick={handleClick}
+            onClick={handleSelectImage}
             className="font-bold px-4 py-2 bg-purple-200 border-2 border-purple-200 text-white cursor-pointer rounded-lg hover:bg-white hover:text-purple-200 duration-300"
           >
             Select Image
@@ -49,7 +73,7 @@ export const AddProduct = ({ cancelAddProduct }) => {
                 type="text"
                 name="productName"
                 id="productName"
-                className="px-3 py-1 border-2 border-black rounded-lg w-full"
+                className="px-3 py-1 border-2 border-black rounded-lg w-full outline-purple-200"
               />
             </div>
             <div className="flex flex-col gap-2 text-xl">
@@ -58,7 +82,7 @@ export const AddProduct = ({ cancelAddProduct }) => {
                 type="text"
                 name="productBrand"
                 id="productBrand"
-                className="px-3 py-1 border-2 border-black rounded-lg w-full"
+                className="px-3 py-1 border-2 border-black rounded-lg w-full outline-purple-200"
               />
             </div>
             <div className="flex flex-col gap-2 text-xl">
@@ -67,7 +91,7 @@ export const AddProduct = ({ cancelAddProduct }) => {
                 type="text"
                 name="flavor"
                 id="flavor"
-                className="px-3 py-1 border-2 border-black rounded-lg w-full"
+                className="px-3 py-1 border-2 border-black rounded-lg w-full outline-purple-200"
               />
             </div>
             <div className="flex flex-col gap-2 text-xl">
@@ -77,14 +101,21 @@ export const AddProduct = ({ cancelAddProduct }) => {
                   type="text"
                   name="size"
                   id="size"
-                  className="px-3 py-1 border-2 border-black rounded-lg w-full"
+                  className="px-3 py-1 border-2 border-black rounded-lg w-full outline-purple-200"
                 />
                 <select
                   name="size"
                   id="size"
-                  className="px-3 py-1 border-2 border-black rounded-lg w-full"
+                  className="px-3 py-1 border-2 border-black rounded-lg w-full outline-purple-200"
                 >
                   <option value="">Select Size</option>
+                  <option value="oz">Oz</option>
+                  <option value="cup">Cup</option>
+                  <option value="pint">Pint</option>
+                  <option value="quart">Quart</option>
+                  <option value="gallon">Gallon</option>
+                  <option value="lbs">Lbs</option>
+                  <option value="liters">Liters</option>
                 </select>
               </div>
             </div>
@@ -97,7 +128,7 @@ export const AddProduct = ({ cancelAddProduct }) => {
                 type="text"
                 name="productDescription"
                 id="productDescription"
-                className="h-[135px] px-3 py-1 border-2 border-black rounded-lg resize-none w-full"
+                className="h-[135px] px-3 py-1 border-2 border-black rounded-lg resize-none w-full outline-purple-200"
               />
             </div>
             <div className="flex gap-5">
@@ -107,7 +138,7 @@ export const AddProduct = ({ cancelAddProduct }) => {
                   type="text"
                   name="productPrice"
                   id="productPrice"
-                  className="px-3 py-1 border-2 border-black rounded-lg w-full"
+                  className="px-3 py-1 border-2 border-black rounded-lg w-full outline-purple-200"
                 />
               </div>
               <div className="flex flex-col gap-2 text-xl">
@@ -116,7 +147,7 @@ export const AddProduct = ({ cancelAddProduct }) => {
                   type="number"
                   name="stock"
                   id="stock"
-                  className="px-3 py-1 border-2 border-black rounded-lg w-full"
+                  className="px-3 py-1 border-2 border-black rounded-lg w-full outline-purple-200"
                 />
               </div>
             </div>
@@ -126,7 +157,7 @@ export const AddProduct = ({ cancelAddProduct }) => {
                 type="text"
                 name="allergens"
                 id="allergens"
-                className="px-3 py-1 border-2 border-black rounded-lg w-full"
+                className="px-3 py-1 border-2 border-black rounded-lg w-full outline-purple-200"
               />
             </div>
           </div>

60423f0558c4f6d740f83a776b2f40dc69268419
nirapyus <vjpcanaria@bpsu.edu.ph>
2024-07-04 13:32:02 +0800

    .

--- client/src/pages/product-details.jsx
+++ client/src/pages/product-details.jsx
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Select from "react-select";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faStar } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faStar,
+  faIceCream,
+  faScroll,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const ProductDetails = () => {
   const { productId } = useParams();
+  const navigate = useNavigate();
   const [product, setProduct] = useState({});
+  const [reviews, setReviews] = useState([]);
   const [sizes, setSizes] = useState([]);
+  const [selectedSize, setSelectedSize] = useState(null);
+  const [price, setPrice] = useState(0);
+  const [stock, setStock] = useState(0);
+  const [activeDescRev, setActiveDescRev] = useState("description");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,6 +39,17 @@ export const ProductDetails = () => {
           );
           if (sizesResponse.data.status === 1) {
             setSizes(sizesResponse.data.products);
+
+            const lowestPriceOption = sizesResponse.data.products.reduce(
+              (min, size) => (size.price < min.price ? size : min),
+              sizesResponse.data.products[0]
+            );
+            setSelectedSize({
+              label: lowestPriceOption.size,
+              value: lowestPriceOption,
+            });
+            setPrice(lowestPriceOption.price);
+            setStock(lowestPriceOption.stock);
           }
         } catch (error) {
           console.error("Error fetching data:", error);
@@ -37,10 +59,32 @@ export const ProductDetails = () => {
 
     fetchData();
   }, [productId]);
+
+  const handleReturnShop = () => {
+    navigate("/shop");
+  };
+
+  const handleSizeChange = (selectedOption) => {
+    setSelectedSize(selectedOption);
+    setPrice(selectedOption.value.price);
+    setStock(selectedOption.value.stock);
+  };
+
+  const handleDescriptionClick = () => {
+    setActiveDescRev("description");
+  };
+
+  const handleReviewsClick = () => {
+    setActiveDescRev("reviews");
+  };
+
   return (
     <div className="mt-20 mb-10 pb-10 min-h-[70vh] flex justify-center gap-36">
       <div className="flex flex-col gap-5">
-        <div className="flex items-center gap-3 text-xl cursor-pointer">
+        <div
+          onClick={handleReturnShop}
+          className="flex items-center gap-3 text-xl cursor-pointer"
+        >
           <FontAwesomeIcon icon={faArrowLeft} />
           <p className="font-semibold">Return to shop</p>
         </div>
@@ -84,35 +128,66 @@ export const ProductDetails = () => {
             </div>
           </div>
 
-          <div className="flex gap-10 text-xl">
+          <div className="flex justify-between items-center text-xl">
             <p>
               <span className="font-bold">Flavor:</span> {product.flavor}
             </p>
             <p>
               <span className="font-bold">Brand:</span> {product.brand}
             </p>
+            <p className="font-bold text-2xl text-right">
+              Php {price.toFixed(2)}
+            </p>
           </div>
 
           <div className="flex items-center justify-between">
-            <Select></Select>
-            <p className="font-semibold text-2xl">Php 120.00</p>
+            <Select
+              className="w-full"
+              value={selectedSize}
+              onChange={handleSizeChange}
+              options={sizes.map((size) => ({
+                label: size.size,
+                value: size,
+              }))}
+            />
+            <p className="w-full text-xl text-right text-gray-200">
+              <FontAwesomeIcon icon={faIceCream} /> {stock} Items Left
+            </p>
           </div>
         </div>
 
         <div className="my-5">
           <div className="flex justify-between pb-5 border-b-2 border-gray-200">
-            <p className="w-full font-semibold text-center text-lg text-purple-200 cursor-pointer">
+            <p
+              onClick={handleDescriptionClick}
+              className={`w-full font-semibold text-center text-lg ${
+                activeDescRev === "description"
+                  ? "text-purple-200"
+                  : "text-gray-200"
+              } cursor-pointer`}
+            >
               Description
             </p>
-            <p className="w-full font-semibold text-center text-lg text-gray-200 cursor-pointer">
-              Ingredients
-            </p>
-            <p className="w-full font-semibold text-center text-lg text-gray-200 cursor-pointer">
+            <p
+              onClick={handleReviewsClick}
+              className={`w-full font-semibold text-center text-lg ${
+                activeDescRev === "reviews"
+                  ? "text-purple-200"
+                  : "text-gray-200"
+              } cursor-pointer`}
+            >
               Reviews
             </p>
           </div>
 
-          <p className="p-5 ">asdasdasda</p>
+          {activeDescRev === "description" ? (
+            <p className="p-5 text-xl text-justify">{product.description}</p>
+          ) : (
+            <div className="h-full flex items-center justify-center text-3xl gap-5">
+              <p className="font-bold">No reviews yet</p>
+              <FontAwesomeIcon icon={faScroll} className="text-purple-200" />
+            </div>
+          )}
         </div>
 
         <div className="mt-auto flex justify-between">

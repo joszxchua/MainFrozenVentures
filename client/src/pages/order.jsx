d089abdddf09d967f97186bf17a91a460e3b25d3
nirapyus <vjpcanaria@bpsu.edu.ph>
2024-07-10 16:18:34 +0800

    .

--- client/src/pages/order.jsx
+++ client/src/pages/order.jsx
@@ -1,12 +1,64 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { UserContext } from "../context/user-context";
 import { OrderContext } from "../context/order-context";
+import municipalitiesInBataan from "../municipalities";
+import Select from "react-select";
+
+const customStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    borderColor: state.isFocused ? "rgb(123, 106, 194)" : provided.borderColor,
+    boxShadow: state.isFocused
+      ? "0 0 0 1px rgb(123, 106, 194)"
+      : provided.boxShadow,
+  }),
+};
 
 export const Order = () => {
   const { user } = useContext(UserContext);
+  const {
+    register: registerAddress,
+    handleSubmit: handleSubmitAddress,
+    setValue: setValueAddress,
+    getValues: getValuesAddress,
+  } = useForm();
   const { orderProducts, clearOrder } = useContext(OrderContext);
   const productsObject = orderProducts?.products || {};
   const productsArray = Object.values(productsObject);
+  const [selectedMunicipality, setSelectedMunicipality] = useState("");
+  const [selectedBarangay, setSelectedBarangay] = useState("");
+  const [barangays, setBarangays] = useState([]);
+
+  useEffect(() => {
+    const selectedMunicipalityObj = municipalitiesInBataan.find(
+      (municipality) => municipality.name === selectedMunicipality
+    );
+
+    if (selectedMunicipalityObj) {
+      setBarangays(
+        selectedMunicipalityObj.barangays.map((barangay) => barangay.name)
+      );
+    } else {
+      setBarangays([]);
+    }
+  }, [selectedMunicipality]);
+
+  useEffect(() => {
+    if (selectedMunicipality && selectedBarangay) {
+      const selectedMunicipalityObj = municipalitiesInBataan.find(
+        (municipality) => municipality.name === selectedMunicipality
+      );
+      if (selectedMunicipalityObj) {
+        const selectedBarangayObj = selectedMunicipalityObj.barangays.find(
+          (barangay) => barangay.name === selectedBarangay
+        );
+        if (selectedBarangayObj) {
+          setValueAddress("zipCode", selectedBarangayObj.zipCode);
+        }
+      }
+    }
+  }, [selectedMunicipality, selectedBarangay, setValueAddress]);
 
   return (
     <div className="mt-20 min-h-[70vh] grid grid-cols-1 md:grid-cols-[70%_30%] px-10 pb-10">
@@ -66,7 +118,125 @@ export const Order = () => {
       </div>
 
       <div className="bg-gray-100 p-10 rounded-lg flex flex-col min-h-60 max-h-fit">
-        <h2 className="text-4xl font-bold mb-4">Order Details</h2>
+        <h2 className="text-4xl font-bold mb-4">Billing</h2>
+
+        <div className="flex flex-wrap justify-between gap-2">
+          <p className="flex flex-col text-xl font-bold">
+            <span className="text-lg text-gray-200 font-semibold">Name:</span>{" "}
+            Vince Jeremy Canaria
+          </p>
+
+          <p className="flex flex-col text-xl font-bold">
+            <span className="text-lg text-gray-200 font-semibold">Email:</span>{" "}
+            customer@gmail.com
+          </p>
+
+          <p className="flex flex-col text-xl font-bold">
+            <span className="text-lg text-gray-200 font-semibold">
+              Phone Number:
+            </span>{" "}
+            09131231
+          </p>
+        </div>
+
+        <form className="flex flex-col items-center gap-5 py-5">
+          <div className="w-full flex flex-col gap-2">
+            <label
+              htmlFor="street"
+              className="text-lg text-gray-200 font-semibold"
+            >
+              Street:
+            </label>
+            <input
+              type="text"
+              name="street"
+              id="street"
+              className="text-lg px-3 py-1 border-[1px] border-gray-200 rounded-[5px] w-full outline-purple-200"
+              {...registerAddress("street")}
+            />
+          </div>
+
+          <div className="w-full flex gap-5">
+            <div className="w-full flex flex-col gap-2">
+              <label
+                htmlFor="municipality"
+                className="text-lg text-gray-200 font-semibold"
+              >
+                Municipality:
+              </label>
+              <Select
+                styles={customStyles}
+                className="text-lg border-gray-200 rounded-[5px] w-full outline-purple-200"
+                options={municipalitiesInBataan.map((municipality) => ({
+                  value: municipality.name,
+                  label: municipality.name,
+                }))}
+                value={{
+                  value: selectedMunicipality,
+                  label: selectedMunicipality,
+                }}
+                onChange={(selectedOption) =>
+                  setSelectedMunicipality(selectedOption.value)
+                }
+              />
+            </div>
+
+            <div className="w-full flex flex-col gap-2">
+              <label
+                htmlFor="barangay"
+                className="text-lg text-gray-200 font-semibold"
+              >
+                Barangay:
+              </label>
+              <Select
+                styles={customStyles}
+                className="text-lg border-gray-200 rounded-[5px] w-full outline-purple-200"
+                options={barangays.map((barangay) => ({
+                  value: barangay,
+                  label: barangay,
+                }))}
+                value={{ value: selectedBarangay, label: selectedBarangay }}
+                onChange={(selectedOption) =>
+                  setSelectedBarangay(selectedOption.value)
+                }
+              />
+            </div>
+          </div>
+
+          <div className="w-full flex gap-5">
+            <div className="w-full flex flex-col gap-2">
+              <label
+                htmlFor="province"
+                className="text-lg text-gray-200 font-semibold"
+              >
+                Province:
+              </label>
+              <input
+                type="text"
+                name="province"
+                id="province"
+                className="text-lg px-3 py-1 border-[1px] border-gray-200 rounded-[5px] w-full outline-purple-200"
+                {...registerAddress("province")}
+              />
+            </div>
+
+            <div className="w-full flex flex-col gap-2">
+              <label
+                htmlFor="zipCode"
+                className="text-lg text-gray-200 font-semibold"
+              >
+                Zip Code:
+              </label>
+              <input
+                type="text"
+                name="zipCode"
+                id="zipCode"
+                className="text-lg px-3 py-1 border-[1px] border-gray-200 rounded-[5px] w-full outline-purple-200"
+                {...registerAddress("zipCode")}
+              />
+            </div>
+          </div>
+        </form>
 
         <div className="mt-auto pt-5">
           <p className="flex justify-between font-semibold text-xl border-b pb-5 mb-5">
